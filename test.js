c9d5493113a105f1174d8561717dae0713280dfa
yefeng <1799914115@qq.com>
2024-10-08 17:45:57 +0800

    222

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 /**
  * cron 0 0 * * *
  */
+import axios from 'axios'
+
 axios({
     url: 'https://wxclub-api.gentlemonster.com/api/sp-portal/store/nowhere/exclusiveActivity/findBookAbleDate?counterId=NW0000002&activityId=6614a22bcd8319df36a7a092&channel=fans'
 }).then*(response => {
