9e62071ebefe6b3bf32c132eedaca1bf53823407
yefeng <1799914115@qq.com>
2024-10-08 17:44:41 +0800

    22

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 /**
  * cron 0 0 * * *
  */
+axios({
+    url: 'https://wxclub-api.gentlemonster.com/api/sp-portal/store/nowhere/exclusiveActivity/findBookAbleDate?counterId=NW0000002&activityId=6614a22bcd8319df36a7a092&channel=fans'
+}).then*(response => {
+    console.log(response);
+    
+})
 console.log('test');
